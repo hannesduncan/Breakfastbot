7d966c8d4864ab0f93efb2a159227f7a1b05480b
= <=>
2016-08-30 12:12:38 +0100

    fixed stuff

--- breakfastbot/breakfastbot/Global.asax.cs
+++ breakfastbot/breakfastbot/Global.asax.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace breakfastbot
@@ -15,7 +14,7 @@ namespace breakfastbot
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
