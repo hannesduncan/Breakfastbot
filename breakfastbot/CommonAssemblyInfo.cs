010d8d10f08eb701f67ae7bd2bb240e7ccc7dcdf
= <=>
2016-08-30 13:10:20 +0100

    changed to 3.1.0

--- breakfastbot/CommonAssemblyInfo.cs
+++ breakfastbot/CommonAssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Reflection;
 //------------------------------------------------------------------------------
 
 [assembly: AssemblyVersion("$(BUILD_NUMBER)")]
-[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
 [assembly: AssemblyCopyrightAttribute("Copyright © Aqueduct 2007-2012")]
 [assembly: AssemblyCompanyAttribute("Aqueduct")]
 [assembly: AssemblyConfiguration("Release")]
